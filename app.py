abd561821064371ad3fd5768a9426d03844ff54b
Ernest Gottschalk <ayrnee@gmail.com>
2018-04-09 17:35:39 -0400

    changed db removed print statements

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask,render_template, request
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'sql9.freemysqlhosting.net'
-app.config['MYSQL_USER'] = 'sql9231640'
-app.config['MYSQL_PASSWORD'] = 'ckaVTchxBC'
-app.config['MYSQL_DB'] = 'sql9231640'
+app.config['MYSQL_USER'] = 'sql9231688'
+app.config['MYSQL_PASSWORD'] = 'H3CG2k3SjY'
+app.config['MYSQL_DB'] = 'sql9231688'
 mysql = MySQL(app)
 
-# tables = {"student":["sid","email","fname","lname"], "university":["univid","univname","city"], "student_university":["univid", "sid"]}
 tables = {}
 def findtables(substr):
     ans = {}
@@ -37,27 +36,18 @@ def index():
     cur.execute('''SELECT table_name FROM information_schema.tables where table_type="base table"''')
     t_names = cur.fetchall()
     i = 0
-    print t_names
     for name in t_names:
-            print name
             str = "DESCRIBE" +' '+ name[0]
-            print str
             cur.execute(str)
             t_vals = cur.fetchall()
             vals = []
-            # print t_vals
 
             for j in range(len(t_vals)):
                 vals.append(t_vals[j][0])
-                print t_vals[j][0]
                 ++j
             tables[name[0]] = vals
             ++i
-    print tables
-    # print str
-    # cur.execute(str)
-    # ans2 = cur.fetchall()
-    # print ans2
+
     if request.method == 'POST':
         target_tables = findtables(request.form['target'])
         return render_template('data.html', target_tables=target_tables)
